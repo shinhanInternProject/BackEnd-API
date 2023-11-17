e623ffd863f269b7bf0e9406de4618c347af8e37
C0012S <alwaysh1002@gmail.com>
2023-11-17 23:17:19 +0900

    [#1] INIT : Project

---
+++ src/main/java/com/linkstock/LinkStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.linkstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkStockApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkStockApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linkstock/LinkStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkStockApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
